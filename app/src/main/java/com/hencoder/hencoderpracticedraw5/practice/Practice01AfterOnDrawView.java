0df7a323e4b1a94c9d66b0e0cdd7141485d1af27
Your Name <you@example.com>
2017-12-14 19:46:19 +0800

    练习完成

--- app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice01AfterOnDrawView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw5/practice/Practice01AfterOnDrawView.java
@@ -9,10 +9,12 @@ import android.graphics.drawable.Drawable;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.util.Log;
 
 import com.hencoder.hencoderpracticedraw5.R;
 
 public class Practice01AfterOnDrawView extends AppCompatImageView {
+    private static final String TAG = Practice01AfterOnDrawView.class.getSimpleName();
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 
     public Practice01AfterOnDrawView(Context context) {
@@ -40,13 +42,14 @@ public class Practice01AfterOnDrawView extends AppCompatImageView {
         // 由于这期的重点是绘制代码的位置而不是绘制代码本身，所以直接给出绘制代码，你只要解除注释就好
         // 爽吧？
 
-        /*Drawable drawable = getDrawable();
+        Drawable drawable = getDrawable();
         if (drawable != null) {
             canvas.save();
             canvas.concat(getImageMatrix());
             Rect bounds = drawable.getBounds();
+            Log.d(TAG, "宽度：" + bounds.width() + " 高度：" + bounds.height());
             canvas.drawText(getResources().getString(R.string.image_size, bounds.width(), bounds.height()), 20, 40, paint);
             canvas.restore();
-        }*/
+        }
     }
 }
